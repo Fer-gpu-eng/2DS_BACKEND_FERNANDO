81a684d1696fd386ba0252e159c32d3bf5dd9fcf
Fer-gpu-eng <fernando.maciozek@escola.pr.gov.br>
2026-02-25 13:28:30 +0000

    variaveis para pessoa

---
+++ variaveis/var-pessoa.js
@@ -0,0 +1,18 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Tony"
+let sobrenome = "Stark"
+const cpf = "99999999-17"
+const rg = "091929 spp"
+let idade = "53"
+let solteiro = true
+let nota  = 9 + 6
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
