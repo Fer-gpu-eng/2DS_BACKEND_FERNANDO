cdb24fa7d80b99ea8a62e31289e5d48d2e518078
Fer-gpu-eng <fernando.maciozek@escola.pr.gov.br>
2026-03-25 11:37:06 +0000

    Finalização do JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,17 @@
+const jogador = {
+    
+    nome: "Kriston",
+    genero: "masculino",
+    idade: 30,
+    profissao: "jogador de futebol",
+    time: "Real Madrid",
+    gols: 269,
+
+}
+
+console.log(jogador.nome)
+console.log(jogador.genero)
+console.log(jogador.idade)
+console.log(jogador.profissao)
+console.log(jogador.time)
+console.log(jogador.gols)
---
+++ objetos/obj-02.js
@@ -0,0 +1,21 @@
+const escola = {
+
+    nome: "Escola João Paulo II",
+    anoFundacao: 1995,
+    tipo: "Pública",
+    diretor: "Silvester",
+    endereco: "Rua das Flores, 123",
+    cidade: "Tingui",
+    estado: "Paraná",
+    telefone: "(11) 1234-5678"
+
+}
+
+console.log("Nome da escola: " + escola.nome)
+console.log("Ano de fundação: " + escola.anoFundacao)
+console.log("Tipo administrativo: " + escola.tipo)
+console.log("Diretor da escola: " + escola.diretor)
+console.log("Endereço: " + escola.endereco)
+console.log("Cidade: " + escola.cidade)
+console.log("Estado: " + escola.estado)
+console.log("Telefone da escola: " + escola.telefone)
