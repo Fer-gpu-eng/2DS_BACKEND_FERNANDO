7d594707bd936c3db3c6091b3b476b48d47ad604
Fer-gpu-eng <fernando.maciozek@escola.pr.gov.br>
2026-09-02 12:30:18 +0000

    Mudancinha pequena

--- 3º TRIMESTRE/express01/servidor.js
+++ 3º TRIMESTRE/express01/servidor.js
@@ -3,11 +3,14 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Home Ok!')
+    res.send(`<h3>Página Inicial</h3>\n
+        <p>Loja fodona de roupas</p>`)
 })
 
 app.get('/roupas', (req, res) => {
     res.send('Minha roupa de sair!')
 })
 
-app.listen(3000, ()=> {console.log('Servidor está vivo')})
+app.listen(3000, ()=> {console.log('Servidor está vivo')
+
+})
